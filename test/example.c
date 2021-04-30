4f49f9be0d02c3d9dbfaeabbcce4efccf287045d
shustrov38 <shustrov38@gmail.com>
2021-04-30 19:40:35 +0300

    updated function utils

--- test/example.c
+++ test/example.c
@@ -1,17 +1,42 @@
-int f() {
-    main();
-    return 1;
+void s() {
+    f();
 }
 
-int myfunc(int x) {
-    int z = 3;
-    if (x < 3)
-        return 0;
-    z++;
-    myfunc(z - 2);
-    return f();
+void g() {
+    s();
+}
+
+int f() {
+    g();
+    for (int i = 0; i < 10; ++i) {
+        for (int i = 0; i < 10; ++i) {
+            for (int i = 0; i < 10; ++i) {
+                for (int i = 0; i < 10; ++i) {
+
+                }
+            }
+        }
+    }
+    return 54;
 }
 
 int main() {
-    int a = 5;
+    for (int i = 0; i < 10; ++i) {
+        for (int i = 0; i < 10; ++i) {
+            for (int i = 0; i < 10; ++i) {
+                for (int i = 0; i < 10; ++i) {
+
+                }
+            }
+        }
+        for (int i = 0; i < 10; ++i) {
+
+        }
+        for (int i = 0; i < 10; ++i) {
+            for (int i = 0; i < 10; ++i) {
+                int a = f();
+            }
+        }
+    }
+    return 0;
 }
